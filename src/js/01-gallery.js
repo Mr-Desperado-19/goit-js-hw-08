1c3193238e00ac744ff924eba29f08a788837711
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-04-14 15:35:12 +0200

    Task 1 - Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,15 +4,18 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-// const gallery = document.querySelector('.gallery');
-//     const markup = galleryItems.map(({preview, original, description}) =>
-// `<li class="gallery__item">
-//    <a class="gallery__link" href='${original}'>
-//       <img class="gallery__image" src = '${preview}' alt='${description}' />
-//    </a>
-// </li>`);
-// gallery.insertAdjacentHTML('beforeend', markup.join(''));
-//      new SimpleLightbox('.gallery a', {
-//         captionsData: "alt",
-//         captionDelay: 250,
-//     });
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+    const markup = galleryItems.map(({preview, original, description}) =>
+`<li class="gallery__item">
+   <a class="gallery__link" href='${original}'>
+      <img class="gallery__image" src = '${preview}' alt='${description}' />
+   </a>
+</li>`);
+gallery.insertAdjacentHTML('beforeend', markup.join(''));
+     new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
