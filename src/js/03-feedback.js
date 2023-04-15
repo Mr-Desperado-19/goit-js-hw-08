60eaa6f071e79b2c94d58ccefb3ce59a903b4c66
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-04-15 15:00:28 +0200

    Task 3 - in process

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+// Відстежуємо подію input на формі
+form.addEventListener('input', throttle(function() {
+  const formData = new FormData(form);
+  const feedback = {
+    email: formData.get('email'),
+    message: formData.get('message')
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedback));
+}, 500));
+
+// Перевіряємо стан сховища під час завантаження сторінки
+const savedFeedback = localStorage.getItem(STORAGE_KEY);
+if (savedFeedback) {
+  const feedback = JSON.parse(savedFeedback);
+  form.elements.email.value = feedback.email;
+  form.elements.message.value = feedback.message;
+  console.log(feedback)
+}
+
+// Очищуємо сховище та поля форми під час сабміту
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+  
+    // Отримуємо поточні значення полів та виводимо їх у консоль
+    const emailInput = form.elements.email;
+    const messageInput = form.elements.message;
+    console.log({
+      email: emailInput.value,
+      message: messageInput.value
+    });
+});
